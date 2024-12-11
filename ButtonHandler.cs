77ebb44848fd050efcfaffce140f182a244196a8
Willian <Willian@USER>
2024-12-11 11:33:17 -0500

    Agregar archivos de proyecto.

---
+++ ButtonHandler.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows;
+
+namespace MiProyectoWPF
+{
+    public class ButtonHandler
+    {
+        public void HandleButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 2 Clicked!");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiProyectoWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly ButtonHandler buttonHandler = new ButtonHandler();
+   
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void button2_Click(object sender, RoutedEventArgs e)
+    {
+        buttonHandler.HandleButtonClick(sender, e);
+    }
+}
